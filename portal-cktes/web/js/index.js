f8f3b21d8f43bb72c0a0eededc5498d403f2671e
AndresK21 <andresdosmil@gmail.com>
2018-03-10 13:41:03 -0600

    Ingreso del portal

---
+++ portal-cktes/web/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+        $('span').click(function() {
+            $('.overlay').toggleClass('anim');
+        });
+
+        $('.animation').click(function(){
+            $('.anim').toggleClass('reverse-animation');
+        });
+
+        $('.slider').slider();
+});
+
