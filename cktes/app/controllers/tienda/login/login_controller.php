f5c6f7dfbccf79a6915ed4ebe858da74292384c2
DGuzman232 <39032095+DGuzman232@users.noreply.github.com>
2018-05-15 10:09:54 -0600

    Cambios necesarios
    ...

---
+++ cktes/app/controllers/tienda/login/login_controller.php
@@ -0,0 +1,3 @@
+<?php
+require_once("../app/views/tienda/login/login_view.php");
+?>
---
+++ cktes/app/views/tienda/login/login_view.php
@@ -0,0 +1,43 @@
+<div>
+    <div class="white-text">.</div>
+    <div class="white-text">.</div>
+    <div class="white-text">.</div>
+    <div class="white-text">.</div>
+    <div class="white-text">.</div>
+    <div class="container"  id="FondoLogin">
+        <ul id="FondoTabLogin" class="tabs tabs-fixed-width tab-demo z-depth-1">
+            <li class="tab">
+                <a class="active" href="#test1">REGISTRARSE</a>
+            </li>
+            <li class="tab">
+                <a href="#test2">INICIAR SESI&Oacute;N</a>
+            </li>
+        </ul>
+        <div id="test1" class="col s12">
+            <h4 class="center white-text">Registrate gratis</h4>
+            <div class="row">
+                    <form class="col s12">
+                        <div class="input-field col s6">
+                          <input id="Nombres" type="text" class="validate transparent">
+                          <label for="last_name">Nombres</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input id="Apellidos" type="text" class="validate transparent">
+                            <label for="last_name">Apellidos</label>
+                         </div>
+                         <div class="input-field col s6">
+                                <input id="email" type="text" class="validate transparent">
+                                <label for="email">Correo electr&oacute;nico</label>
+                              </div>
+                              <div class="input-field col s6">
+                                    <input id="email" type="text" class="validate transparent">
+                                    <label for="email">Correo electrónico</label>
+                                  </div>
+                      </div>
+                     
+        </div>
+        <div id="test2" class="col s12">
+            <p>Test 2</p>
+        </div>
+    </div>
+</div>
