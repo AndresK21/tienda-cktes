39efc74dc6465b12c6bb18ef233033d001ba61a4
DGuzman232 <39032095+DGuzman232@users.noreply.github.com>
2018-05-14 20:01:05 -0600

    Cosas innecesarias
    xdxdxdxd

--- cktes/app/view/nav_tienda.php
+++ cktes/app/view/nav_tienda.php
@@ -1,23 +1,38 @@
 <div class="navbar-fixed">
-<nav>
+  <nav>
     <div class="nav-wrapper">
-      <a href="../portal/index.php"><span>CKT</span> <span>ES</span></a>
+      <a href="../portal/index.php">
+        <span>CKT</span>
+        <span>ES</span>
+      </a>
     </div>
     <div class="sandwich" id="btn">
       <span class="top"></span>
       <span class="middle"></span>
       <span class="bottom"></span>
     </div>
-    <a href="../tienda/carrito.php"> <span  class="iconomenu"><i class="fas fa-shopping-cart tma"></i></span></a>
+    <a href="../tienda/carrito.php">
+      <span class="iconomenu">
+        <i class="fas fa-shopping-cart tma"></i>
+      </span>
+    </a>
     <div class="menu" id="menu">
       <div class="menu-list">
         <ul class="row">
-          <li class="col s12"><a href="../portal/index.php">CKTES</a></li>
-          <li class="col s12"><a href="../tienda/index.php">Tienda</a></li>
-          <li class="col s12"><a href="../tienda/categoria.php">Categoria</a></li>
-          <li class="col s12"><a href="../tienda/servicios.php">Servicios</a></li>
+          <li class="col s12">
+            <a href="../portal/index.php">CKTES</a>
+          </li>
+          <li class="col s12">
+            <a href="../tienda/index.php">Tienda</a>
+          </li>
+          <li class="col s12">
+            <a href="../tienda/categoria.php">Categoria</a>
+          </li>
+          <li class="col s12">
+            <a href="../tienda/servicios.php">Servicios</a>
+          </li>
         </ul>
       </div>
     </div>
-</nav>
+  </nav>
 </div>
