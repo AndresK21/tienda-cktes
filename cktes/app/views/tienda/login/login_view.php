a9bc9d160df2adb95696e204f67ebb5eb0f895e6
Fátima <Fatimaaguilar7@hotmail.com>
2018-05-19 18:59:22 -0600

    Prueba
    ...

--- cktes/app/views/tienda/login/login_view.php
+++ cktes/app/views/tienda/login/login_view.php
@@ -52,13 +52,11 @@
                         <div class="transparent-text">.</div>
                         <div class="transparent-text">.</div>
                     </div>
-                    
                 </form>
             </div>
-
         </div>
         <div id="test2" class="col s12 container">
-            <h4 class="center white-text">Bienvenido Otra Vez</h4>
+            <h4 class="center white-text">Bienvenido Otra Vez!</h4>
             <div class="row">
             <form class="col s12">
                 <div class="input-field col l12 s12">
