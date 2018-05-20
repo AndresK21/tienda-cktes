695c0cb8a6f55cfae7a31414c4aad67a9d132a20
DGuzman232 <39032095+DGuzman232@users.noreply.github.com>
2018-05-19 19:16:47 -0600

    Prueba4
    xdxdxd

--- cktes/app/views/tienda/login/login_view.php
+++ cktes/app/views/tienda/login/login_view.php
@@ -56,7 +56,7 @@
             </div>
         </div>
         <div id="test2" class="col s12 container">
-            <h4 class="center white-text">¡Bienvenido Otra Vez!</h4>
+            <h4 class="center white-text">&#161;Bienvenido Otra Vez&#33;</h4>
             <div class="row">
             <form class="col s12">
                 <div class="input-field col l12 s12">
