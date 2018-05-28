49a247fa7388e07745dbc6906663e1459aecbd66
Fátima <Fatimaaguilar7@hotmail.com>
2018-05-28 10:56:45 -0600

    mira Diego

--- cktes/tienda/acceder.php
+++ cktes/tienda/acceder.php
@@ -3,7 +3,5 @@ require_once("../app/views/tienda/templates/page.class.php");
 Page::templateHeader("Acceder");
 require_once("../app/controllers/tienda/account/login_controller.php");
 Page::templateFooter();
-$_SESSION['id_cliente'] = 4;
-$_SESSION['correo_electronico'] = "ggf@gmal.com";
-$_SESSION['id_carrito'] = 3;
+
 ?>
