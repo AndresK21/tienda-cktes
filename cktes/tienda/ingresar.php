44e2418a910a9bca9037d1c69bd11585a61b59bb
Fátima <Fatimaaguilar7@hotmail.com>
2018-05-27 21:52:48 -0600

    otra parte

--- cktes/tienda/ingresar.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("../app/views/tienda/templates/page.class.php");
-Page::templateHeader("Acceder");
-require_once("../app/controllers/tienda/account/ingresar_controller.php");
-Page::templateFooter();
-?>
