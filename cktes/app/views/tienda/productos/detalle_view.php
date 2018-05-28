ea6ffbdf37d25904106dd272e06badea74a69984
Fátima <Fatimaaguilar7@hotmail.com>
2018-05-28 13:35:09 -0600

    Revert "Mejora"
    This reverts commit 70019b626042b0850cb685becc8ceb9e315141f2.

--- cktes/app/views/tienda/productos/detalle_view.php
+++ cktes/app/views/tienda/productos/detalle_view.php
@@ -7,51 +7,70 @@
 <h2 class="center cktes-text">
     <b>Detalles</b>
 </h2>
-<div class='row'>
-    <form class="col s12" method="post" enctype='multipart/form-data'>
-        <?php
-        print("
-        <h3 class='center'>".$producto->getNombre()."</h3>
-            <div class= col s12 m10 l4>
+<div class='container'>
+    <div class='row'>
+        <div class='col s12'>
+        <form class="col s12" method="post" enctype='multipart/form-data'>
+            <?php
+            print("
+            <h3 class='header'>".$producto->getNombre()."</h3>
                 <div class='card horizontal'>
                     <div class='card-image'>
-                        <img class='materialboxed' width='650' height='280' src='../web/img/productos/".$producto->getImagen()."'>
+                    <img src='../web/img/productos/".$producto->getImagen()."'>
                     </div>
                     <div class='card-stacked'>
-                        <div class='card-content'>
-                            <p><b>Precio (US$) ".$producto->getPrecio()."</b></p>
-                            <div class='card-action'>
-                                <button type='submit' name='agregar' class='btn waves-effect waves-light cktes tooltipped' data-tooltip='Realizar Compra'>
-                                <i class='material-icons'>add_shopping_cart</i>A&ntilde;adir al Carrito</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                    <div class='card-content'>
+                    <p><b>Precio (US$) ".$producto->getPrecio()."</b></p>
+            ");
+            if(isset($_SESSION['id_cliente'])){
+            print(" 
+            <button type='submit' name='agregar' class='btn waves-effect waves-light cktes tooltipped' data-tooltip='Realizar Compra'>
+            <i class='material-icons'>add_shopping_cart</i>A&ntilde;adir al Carrito</button>
+        </div>
+    </div>
+</div>
+</form>
+");
+}
+else{
+    print("
+
+        <div class='row'>
+        <div class='col s12 center'>
+        <span class='cyan-text'>NO PUEDE AÑADIR AL CARRITO SIN INICIAR SESION</span>
+        </div>
+        </div>
         ");
-        ?>
-        <!-- ESTRELLAS -->
-        <?php
-            foreach($valoracion3 as $valoracionesE){
-            print("  
-            <h5><p class='center'>Valoración Actual: $valoracionesE[Estrellas] estrellas</p></h5>
-            ");
-            }
-        ?>
-        <!-- COMENTARIOS -->
-        <?php
-            foreach($valoracion2 as $valoracionesR){
-            print("  
-            <ul class='collection'>
-                <li class='collection-header'><h5></h5></li>
-                <li class='collection-item avatar'>
-                <i class=' cktes material-icons circle'>comment</i>
-                <span class='card-title'>$valoracionesR[correo_electronico]</span>
-                <p>$valoracionesR[comentario]</p>
-                </li>
-            </ul>
-            ");
-            }
-        ?>
-    </form>
+}
+?>
+<!-- ESTRELLAS -->
+<?php
+    foreach($valoracion3 as $valoracionesE){
+    print("  
+    <div class='card-reveal'>
+    <h5><p class='center'>Valoración Actual: $valoracionesE[Estrellas] estrellas</p></h5>
+    </div>
+    ");
+    }
+?>
+<!-- COMENTARIOS -->
+<h5>
+    <p class="center">Comentarios</p>
+</h5>
+<?php
+    foreach($valoracion2 as $valoracionesR){
+    print("  
+    <ul class='collection'>
+        <li class='collection-header'><h5></h5></li>
+        <li class='collection-item avatar'>
+        <i class='material-icons circle'>comment</i>
+        <span class='card-title'>$valoracionesR[correo_electronico]</span>
+        <p>$valoracionesR[comentario]</p>
+        </li>
+    </ul>
+    ");
+    }
+?>
+</div>
+</div>
 </div>
