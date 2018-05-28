950109f2f8536113a6914d676bd99f252983b92c
Fátima <Fatimaaguilar7@hotmail.com>
2018-05-28 15:11:50 -0600

    historial detalle compra

---
+++ cktes/app/controllers/tienda/historial/historialdetalle_controller.php
@@ -0,0 +1,23 @@
+<?php
+//Controlador para ver el carrito
+    require_once("../app/models/detalle.class.php");
+    if(isset($_SESSION['id_carrito'])){
+	try{	
+			$detalle = new Detalle;
+			// Se obtiene el id pdel cliente 
+			if($detalle->setCompra($_GET['id'])){
+				//Se obtienen los detalles del historial de compra
+				$detalles= $detalle->readHistorialdetalle();
+				if($detalles){
+					 require_once("../app/views/tienda/historial/historialdetalle_view.php");
+				}else{
+					throw new Exception("No hay productos en el carrito");
+				}
+			}else{
+				throw new Exception("Cliente incorrecto");
+			}
+		}catch(Exception $error){
+		Page::showMessage(3, $error->getMessage(), "principal.php");
+    }
+}
+?>
