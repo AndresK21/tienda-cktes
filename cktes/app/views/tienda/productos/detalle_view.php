db003519daac501d67655f6bda5fc81513dfbea4
DGuzman23 <36141469+DGuzman23@users.noreply.github.com>
2018-05-28 21:16:58 -0600

    Comentarios v2

--- cktes/app/views/tienda/productos/detalle_view.php
+++ cktes/app/views/tienda/productos/detalle_view.php
@@ -7,79 +7,77 @@
 <h2 class="center cktes-text">
     <b>Detalles</b>
 </h2>
-<div class='container'>
-    <div class='row'>
-        <div class='col s12'>
-        <form class="col s12" method="post" enctype='multipart/form-data'>
-            <?php
-            print("
-            <h3 class='header'>".$producto->getNombre()."</h3>
-                <div class='card horizontal'>
-                    <div class='card-image'>
-                    <img src='../web/img/productos/".$producto->getImagen()."'>
-                    </div>
-                    <div class='card-stacked'>
-                    <div class='card-content'>
-                    <p><b>Precio (US$) ".$producto->getPrecio()."</b></p>
-            ");
+<?php
+    print("
+        <h3 class='center'>".$producto->getNombre()."</h3>
+    ")
+?>
+<div class='row'>
+    <form method="post">
+    <?php
+    foreach($valoracion3 as $valoracionesE){
+    print("
+    <div class='col s12 m12 l8 offset-l2'>
+        <div class='card horizontal'>
+            <div class='card-image'>
+            <img height='300' width='250' src='../web/img/productos/".$producto->getImagen()."'>
+            </div>
+            <div class='card-stacked'>
+    ");
             if(isset($_SESSION['id_cliente'])){
             print(" 
-         <div class='card-action'>
-                            <form method='post'>
-                                <div class='row center'>
-                                    <div class='input-field col s12 m6'>
-                                        <i class='material-icons prefix'>list</i>
-                                        <input id='cantidad' type='number' name='cantidad' min='1' max=".$producto->getExistencias()." class='validate'>
-                                        <label for='cantidad'>Cantidad</label>
-                                    </div>
-                                    <div class='input-field col s12 m6 l12 '>
-                                    <button type='submit' name='agregar' class='btn waves-effect waves-light cktes tooltipped' data-tooltip='Realizar Compra'>
-                                    <i class='material-icons'>add_shopping_cart</i>A&ntilde;adir al Carrito</button>
-                                    </div>
-                            </div>
-                        </form>
+            <div class='card-content'>
+                <h5>".$producto->getDescripcion()."</h5>
+                <h5>Precio (US$): ".$producto->getPrecio()."</h5>
+                <div class=' input-field col s12 m12 l12'>
+                    <input id='cantidad' type='number' name='cantidad' min='1' max=".$producto->getExistencias()." class='validate'>
+                    <label for='cantidad'>Cant</label>
+                </div>
+                <h5><p class='center'>Valoración Actual: $valoracionesE[Estrellas] estrellas</p></h5>
+            </div>
+            <div class='card-action'>
+                <div class='center'>
+                    <div class='input-field col s12 m12 l12 '>
+                        <button type='submit' name='agregar' class=' TamañodelBotonLogin btn waves-effect waves-light cktes tooltipped ' data-tooltip='Realizar Compra' data-position='right'>
+                        A&ntilde;adir al Carrito</button>
                     </div>
-");
-}
-else{
-    print("
-
-        <div class='row'>
-        <div class='col s12 center'>
-        <span class='cyan-text'>NO PUEDE AÑADIR AL CARRITO SIN INICIAR SESION</span>
+                </div>
+            </div>
+            </div>
         </div>
-        </div>
-        ");
-}
-?>
-<!-- ESTRELLAS -->
-<?php
-    foreach($valoracion3 as $valoracionesE){
-    print("  
-    <div class='card-reveal'>
-    <h5><p class='center'>Valoración Actual: $valoracionesE[Estrellas] estrellas</p></h5>
     </div>
-    ");
-    }
-?>
+            ");
+            }
+            else{
+                print("
+                    <div class='row'>
+                    <div class='col s12 center'>
+                    <span class='cyan-text'>NO PUEDE AÑADIR AL CARRITO SIN INICIAR SESION</span>
+                    </div>
+                    </div>
+                    ");
+                }
+            }
+            ?>
+    </form>
+</div>
+
 <!-- COMENTARIOS -->
-<h5>
-    <p class="center">Comentarios</p>
-</h5>
-<?php
-    foreach($valoracion2 as $valoracionesR){
-    print("  
-    <ul class='collection'>
-        <li class='collection-header'><h5></h5></li>
-        <li class='collection-item avatar'>
-        <i class='material-icons circle'>comment</i>
-        <span class='card-title'>$valoracionesR[correo_electronico]</span>
-        <p>$valoracionesR[comentario]</p>
-        </li>
-    </ul>
-    ");
-    }
-?>
-</div>
-</div>
+<div class="row">
+    <div class="col s12 m12 l8 offset-l2">
+    <ul class="collection with-header">
+        <li class="collection-header center"><h4>Comentarios</h4></li>
+    <?php
+        foreach($valoracion2 as $valoracionesR){
+        print(" 
+            <li class='collection-item avatar'>
+            <i class='cktes material-icons circle'>comment</i>
+            <span class='card-title'><b>$valoracionesR[correo_electronico]</b></span>
+            <p>$valoracionesR[comentario]</p>
+            </li>
+        </ul>
+    </div>
+        ");
+        }
+    ?>
 </div>
