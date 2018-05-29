26c859cc1fa729de6174fea9afc77b943ad9a6af
DGuzman23 <36141469+DGuzman23@users.noreply.github.com>
2018-05-29 07:34:59 -0600

    btn

--- cktes/app/views/tienda/valoracion/valoracion_view.php
+++ cktes/app/views/tienda/valoracion/valoracion_view.php
@@ -34,7 +34,7 @@
                     <div class="input-field col s12 l6">
                         <input id="hola" type="text" name="comentario"     >
                         <label for="hola">Comentar</label>
-                        <button class="btn tooltipped waves-effect waves-light" type="submit" name="valorar" data-position="botton" data-delay="50" data-tooltip="Enviar Comentario">Enviar</button>
+                        <button class="btn cktes tooltipped waves-effect waves-light" type="submit" name="valorar" data-position="botton" data-delay="50" data-tooltip="Enviar Comentario">Enviar</button>
                     </div>
                 </div>
         </div>
