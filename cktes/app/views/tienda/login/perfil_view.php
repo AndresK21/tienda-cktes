c0d1274882d4fa8d1922382197fa5eb715faa969
Fátima <Fatimaaguilar7@hotmail.com>
2018-09-10 15:47:26 -0600

    ultimos cambios en clase

--- cktes/app/views/tienda/login/perfil_view.php
+++ cktes/app/views/tienda/login/perfil_view.php
@@ -2,34 +2,46 @@
 <?php
 print("
 <h2 class='center-align truncate'><b>INFORMACIÓN</b></h2>
-<div class='col s12 m8 offset-m2 l6 offset-l3'>
-          <div class='row valign-wrapper'>
-            <div class='col s2'>
-              <img src='../web/img/clientes/$_SESSION[imagen]' alt='' class='circle responsive-img'> <!-- notice the 'circle' class -->
+<div class='container'>
+
+
+
+                
+               
             </div>
-            <div class='col s10'>
-              <span class='black-text'>
-                This is a square image. Add the 'circle' class to it to make it appear circular.
-              </span>
+            <div class='center'>
+                 <img src='../web/img/clientes/$_SESSION[imagen]' alt='' class='circle maxW'> 
             </div>
-          </div>
-      </div>
+
+
+
+
+
+            <div class='file-field input-field col s12 m6 l6'>
+            <div class='center'>
+                <span>Cambiar foto de perfil</span>
+                <input type='file' name='archivo'/>
+            </div>
+            <div class='file-path-wrapper'>
+                <input type='hidden' class='file-path validate' placeholder='Seleccione una imagen'/>
+            </div>
+            
+            <div class='center'>
+                  <h4><b>$_SESSION[nombres2] $_SESSION[apellidos2]</b></h4>
+            </div>
+            <div class='center'>
+                 <h5>$_SESSION[correo_electronico]</h5>
+            </div>
+</div>
+     
 ");
 ?>
-
-
-
-
-
-
-
-
-
-  
+<div class='fondoEdit'>
 <div class='container'>
    <form method='post'>
         <div class='row'>
             <!--Nombres-->
+            <br>
             <div class='input-field col s12 m6'>
                 <i class='material-icons prefix'>person</i>
                 <input id='nombres' type='text' autocomplete='off' name='nombres' class='validate' value='<?php print($cliente->getNombres()) ?>' required/>
@@ -53,4 +65,5 @@ print("
             <button type='submit' name='editar' class='btn waves-effect blue tooltipped' data-tooltip='Editar'><i class='material-icons'>save</i></button>
         </div>
    </form>
+</div>
 </div>
