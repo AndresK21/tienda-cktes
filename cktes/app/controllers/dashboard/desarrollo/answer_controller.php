54d3f31c30ac5d8ed254ac6224b852093ffce3f9
andresdosmil@gmail.com <makina8950>
2018-09-17 21:08:42 -0600

    Ya lo ultimo

--- cktes/app/controllers/dashboard/desarrollo/answer_controller.php
+++ cktes/app/controllers/dashboard/desarrollo/answer_controller.php
@@ -1,11 +1,58 @@
 <?php
 require_once("../../app/models/desarrollo.class.php");
+require_once("../../app/models/cliente.class.php");
+require_once("../../app/PHPMailer/class.phpmailer.php");
+require_once("../../app/PHPMailer/class.smtp.php");
 try{
     if(isset($_GET['id'])){ //Llama al id de la maraca
         if($_SERVER['HTTP_REFERER']){
             $desarrollo = new Desarrollo;
             if($desarrollo->setId_Desarrollo($_GET['id'])){ //Establece el id en una varible para usarla despues
                 if($desarrollo->readDesarrollo()){
+
+                    $empleado = new Empleado;
+                    if(isset($_POST['enviar'])){
+                    $_POST = $empleado->validateForm($_POST);
+                        if($empleado->setCorreo($_POST['correo'])){
+                            if($empleado->checkCorreo()){
+                                $nombres = $empleado->getNombres();
+                                $apellidos = $empleado->getApellidos();
+
+                                $mail = new PHPMailer;
+                                $mail->setLanguage('es', '../../app/PHPMailer/language/phpmailer.lang-es.php');
+                                $correo = $_POST['correo'];
+                                $usuario = $nombres." ".$apellidos;
+
+                                $mail->isSMTP();
+                                $mail->Host = 'smtp.gmail.com';
+                                $mail->SMTPAuth = true;
+                                $mail->Username = 'pinturasv503@gmail.com';
+                                $mail->Password = 'pinturasv';
+                                $mail->SMTPSecure = 'ssl';
+                                $mail->Port = 465;
+                                $mail->isHTML(true);
+
+                                $mail->setFrom('pinturasv503@gmail.com', 'CKTES');
+                                $mail->addAddress($correo, $usuario);
+
+                                $mail->Subject = $_POST['asunto'];
+                                $mail->Body = $_POST['mensaje'];
+
+                                if(!$mail->send()){
+                                    Page::showMessage(2, "Error, mensaje no enviado. Error: ".$mail->ErrorInfo, "correo.php");
+                                }else{
+                                    Page::showMessage(1, "El mensaje se ha enviado correctamente", "login.php");
+                                }
+
+                            }else{
+                                throw new Exception("Usuario inexistente");
+                            }
+                        }else{
+                            throw new Exception("Correo electronico incorrecto");
+                        }
+                    }
+
+
                 }else{
                     Page::showMessage(2, "Desarrollo inexistente", "index.php");
                 }
@@ -22,4 +69,3 @@ try{
     Page::showMessage(2, $error->getMessage(), null);
 }
 require_once("../../app/views/dashboard/desarrollo/answer_view.php");
-?>
--- cktes/app/views/dashboard/desarrollo/answer_view.php
+++ cktes/app/views/dashboard/desarrollo/answer_view.php
@@ -5,26 +5,26 @@
     <form>
         <div class="row">
             <div class="input-field col s12">
-                <input id="corre" name="correo" type="email" class="validate" autocomplete="off" value="<?php print($desarrollo->getCorreo())?>">
+                <input id="disabled" name="correo" type="email" class="validate" autocomplete="off" disabled value="<?php print($desarrollo->getCorreo())?>">
                 <label for="corre" class="black-text">Correo electr&oacute;nico</label>
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12">
-                <input id="asunto" type="text" class="validate" autocomplete="off">
-                <label for="asunto" class="black-text">Asunto</label>
+                <input id="asunt" name="asunto" type="text" class="validate" autocomplete="off" required>
+                <label for="asunt" class="black-text">Asunto</label>
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12">
-                <textarea id="mensaje" type="text" class="materialize-textarea" autocomplete="off"></textarea>
-                <label for="mensaje" class="black-text">Mensaje</label>
+                <textarea id="mensaj" name="mensaje" type="text" class="materialize-textarea" autocomplete="off" required></textarea>
+                <label for="mensaj" class="black-text">Mensaje</label>
             </div>
         </div>
         <div class="row">
             <div class="col s12 right-align">
                 <a class='btn waves-effect red darken-3' href="index.php"><i class='material-icons'></i>Cancelar</a>
-                <button type='submit' name='actualizar' class='btn waves-effect blue-grey darken-4'>Enviar mensaje</button>
+                <button type='submit' name='enviar' class='btn waves-effect blue-grey darken-4'>Enviar mensaje</button>
             </div>
         </div>
     </form>
