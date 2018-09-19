19ae46ff8b34482c3d1dfd7395389515496bd3f6
Fátima <Fatimaaguilar7@hotmail.com>
2018-09-18 18:21:05 -0600

    pcb

--- cktes/app/views/portal/pcb/pcb_view.php
+++ cktes/app/views/portal/pcb/pcb_view.php
@@ -2,8 +2,9 @@
 <img class="responsive-img indeximage " src="../web/img/2.jpg"> 
     <h2 class="center"><b>PCB</b></h2>
         <ul id="tabs-swipe-demo" class="tabs center">
-            <li class="tab col s3 "><a href="#test-swipe-1" class ="indigo-text darken-4"><h5>PCB</h5></a></li>
-            <li class="tab col s3"><a href="#test-swipe-2" class ="indigo-text darken-4"><h5>DUDAS</h5></a></li>
+            <li class="tab col s3 "><a href="#test-swipe-1" class ="indigo-text darken-4"><h5> VISUALIZADOR</h5></a></li>
+            <li class="tab col s3 "><a href="#test-swipe-2" class ="indigo-text darken-4"><h5>PCB</h5></a></li>
+            <li class="tab col s3"><a href="#test-swipe-3" class ="indigo-text darken-4"><h5>DUDAS</h5></a></li>
         </ul>
         <div id="test-swipe-1" class="col s12 m6 l3">
             <div class ="container">
@@ -28,9 +29,7 @@
                         </iframe> 
                         </div>
                         </div>
-
-
-                        
+                
                     </div>
                     <h6><b>Especificaciones</b></h6>  
                     <div class='input-field col s12 m6'>
@@ -49,19 +48,22 @@
                 
             </div>
         </div>
-        <div id="test-swipe-2" class="col s12 m6 l3">
+        <div id="test-swipe-3" class="col s12 m6 l3">
             <div class ="container">
             <br>
+            <div class="col s12 m6 l5 push-l1">
+                        <div class="card card-panel hoverable">
             <h5 class="center indigo-text darken-4"><b>Dejanos tus dudas</b></h5> 
                 <form>
-                    <h6><b>Correo</b></h6> 
-                    <input id='correo' type='email' name='correo' class='validate'/>
+                   
                     <h6><b>Asunto</b></h6> 
                     <input id='asunto' name='asunto' class='validate'/>
                     <h6><b>Mensaje</b></h6>
                     <textarea id="textarea1" class="materialize-textarea"></textarea>
                     <a class="waves-effect waves-light btn-large indigo darken-4 right"><i class ="material-icons right">send</i><b>Enviar</b></a><br><br><br><br><br>
                 </form>
+            </div>
+            </div>
             </div>
         </div>
 </main>
