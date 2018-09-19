5fdbbfd5d74cf9c513b6c34498ab8f91d1cd11b6
Fátima <Fatimaaguilar7@hotmail.com>
2018-09-18 20:05:21 -0600

    ya esta lo de dudas

---
+++ cktes/app/controllers/portal/pcb/mensaje_controller.php
@@ -0,0 +1,47 @@
+<?php
+require_once("../app/models/cliente.class.php");
+require_once("../app/PHPMailer/class.phpmailer.php");
+require_once("../app/PHPMailer/class.smtp.php");
+
+try{
+    $cliente = new Cliente;
+    if(isset($_POST['enviar'])){
+    $_POST = $cliente->validateForm($_POST);
+        if($cliente->setCorreo($_SESSION['correo_electronico'])){
+
+                    $nombres = $cliente->getNombres();
+                    $apellidos = $cliente->getApellidos();
+                    $asunto = $_POST['Asunto'];
+                    $duda= $_POST['duda'];
+                    $mail = new PHPMailer;
+                    $mail->setLanguage('es', '../../app/PHPMailer/language/phpmailer.lang-es.php');
+                    $usuario = $nombres." ".$apellidos;
+                    $mail->isSMTP();
+                    $mail->Host = 'smtp.gmail.com';
+                    $mail->SMTPAuth = true;
+                    $mail->Username = 'pinturasv503@gmail.com';
+                    $mail->Password = 'pinturasv';
+                    $mail->SMTPSecure = 'ssl';
+                    $mail->Port = 465;
+                    $mail->setFrom('pinturasv503@gmail.com', 'CKTES');
+                    $mail->addAddress('fatimmaaguilar7@gmail.com', $usuario);
+                    $mail->isHTML(true);
+                    $mail->Subject = 'Duda sobre manufacturacion. ' .$asunto.'';
+                    $mail->Body = ' Cliente: '.$_SESSION['correo_electronico'].' ha enviado el mensaje: '.$duda.'';
+                     
+                    if(!$mail->send()){
+                        Page::showMessage(2, "Error, mensaje no enviado. Error: ".$mail->ErrorInfo, "correo.php");
+                    }else{
+                        Page::showMessage(1, "El mensaje se ha enviado correctamente", "index.php");
+                    }
+
+           
+        }else{
+            throw new Exception("Correo electronico incorrecto");
+        }
+    }
+}catch(Exception $error){
+    Page::showMessage(2, $error->getMessage(), null);
+}
+require_once("../app/views/portal/pcb/pcb_view.php");
+?>
--- cktes/portal/pcb.php
+++ cktes/portal/pcb.php
@@ -2,5 +2,6 @@
 require_once("../app/views/portal/templates/page.class.php");
 Page::templateHeader("PCB");
 require_once("../app/controllers/portal/pcb/pcb_controller.php");
+require_once("../app/controllers/portal/pcb/mensaje_controller.php");
 Page::templateFooter(); 
 ?>
