09c3528090352b1a7c27363b239761eba3166778
Fátima <Fatimaaguilar7@hotmail.com>
2018-09-24 14:04:54 -0600

    last

--- cktes/app/views/dashboard/cuenta/correo_view.php
+++ cktes/app/views/dashboard/cuenta/correo_view.php
@@ -1,18 +1,18 @@
+<div id="fondoTotalDash">
 <div class="white-text">.</div>
 <div class="white-text">.</div>
-
 <form method="post" enctype='multipart/form-data'>
 	<div class="container">
 		<div class="row">
 			<div class="col s12 m6 l6 offset-l3 offset-m3">
-				<div class="card grey lighten-4">
+				<div class="card fondoInicioC">
 					<div class="card-content">
-						<span class="card-title"><h4>Recuperar contrase&ntilde;a</h4></span>
+						<span class="card-title center white-text"><h4>Recuperar contrase&ntilde;a</h4></span>
 						<div class="center-align">
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input id="cmail" type="email" name="correo" class="validate" autocomplete="off" required>
-                                    <label for="cmail" class="black-text">Correo electr&oacute;nico</label>
+                                    <input id="cmail" type="email" name="correo" class="validate white-text" autocomplete="off" required>
+                                    <label for="cmail" class="white-text">Correo electr&oacute;nico</label>
                                 </div>
                             </div>
                             
@@ -20,8 +20,8 @@
 						<div class="white-text">.</div>
                         <div class="row">
                             <div class="col s12 right-align">
-                                <a class='btn waves-effect red darken-3' href="login.php"><i class='material-icons'></i>Cancelar</a>
-                                <button type='submit' name='enviar' class='btn waves-effect blue-grey darken-4'>Enviar</button>
+                                <a class='btn waves-effect cktes2' href="login.php"><i class='material-icons'></i>Cancelar</a>
+                                <button type='submit' name='enviar' class='btn waves-effect colorNa darken-4'>Enviar</button>
                             </div>
                         </div>
 					</div>
@@ -30,3 +30,4 @@
 		</div>
 	</div>
 <form>
+</div>
