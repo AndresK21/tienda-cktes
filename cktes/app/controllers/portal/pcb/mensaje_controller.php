345ec824c040bc89b1fb513319fdd01c49e3aba2
andresdosmil@gmail.com <makina8950>
2018-10-21 19:38:17 -0600

    x4...

--- cktes/app/controllers/portal/pcb/mensaje_controller.php
+++ cktes/app/controllers/portal/pcb/mensaje_controller.php
@@ -9,31 +9,31 @@ try{
     $_POST = $cliente->validateForm($_POST);
         if($cliente->setCorreo($_SESSION['correo_electronico'])){
 
-                    $nombres = $cliente->getNombres();
-                    $apellidos = $cliente->getApellidos();
-                    $asunto = $_POST['Asunto'];
-                    $duda= $_POST['duda'];
-                    $mail = new PHPMailer;
-                    $mail->setLanguage('es', '../../app/PHPMailer/language/phpmailer.lang-es.php');
-                    $usuario = $nombres." ".$apellidos;
-                    $mail->isSMTP();
-                    $mail->Host = 'smtp.gmail.com';
-                    $mail->SMTPAuth = true;
-                    $mail->Username = 'pinturasv503@gmail.com';
-                    $mail->Password = 'pinturasv';
-                    $mail->SMTPSecure = 'ssl';
-                    $mail->Port = 465;
-                    $mail->setFrom('pinturasv503@gmail.com', 'CKTES');
-                    $mail->addAddress('fatimmaaguilar7@gmail.com', $usuario);
-                    $mail->isHTML(true);
-                    $mail->Subject = 'Duda sobre manufacturacion. ' .$asunto.'';
-                    $mail->Body = ' Cliente: '.$_SESSION['correo_electronico'].' ha enviado el mensaje: '.$duda.'';
-                     
-                    if(!$mail->send()){
-                        Page::showMessage(2, "Error, mensaje no enviado. Error: ".$mail->ErrorInfo, "correo.php");
-                    }else{
-                        Page::showMessage(1, "El mensaje se ha enviado correctamente", "pcb.php");
-                    }
+            $nombres = $cliente->getNombres();
+            $apellidos = $cliente->getApellidos();
+            $asunto = $_POST['Asunto'];
+            $duda= $_POST['duda'];
+            $mail = new PHPMailer;
+            $mail->setLanguage('es', '../../app/PHPMailer/language/phpmailer.lang-es.php');
+            $usuario = $nombres." ".$apellidos;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'pinturasv503@gmail.com';
+            $mail->Password = 'pinturasv';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+            $mail->setFrom('pinturasv503@gmail.com', 'CKTES');
+            $mail->addAddress('pinturasv503@gmail.com', $usuario);
+            $mail->isHTML(true);
+            $mail->Subject = 'Duda sobre manufacturacion. ' .$asunto.'';
+            $mail->Body = ' Cliente: '.$_SESSION['correo_electronico'].' ha enviado el mensaje: <br>'.$duda.'';
+                
+            if(!$mail->send()){
+                Page::showMessage(2, "Error, mensaje no enviado. Error: ".$mail->ErrorInfo, "correo.php");
+            }else{
+                Page::showMessage(1, "El mensaje se ha enviado correctamente", "pcb.php");
+            }
 
            
         }else{
